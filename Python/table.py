fe4100b5f34776103750c600faaff121276efdb3
Yar-Ber <sivachev15@gmail.com>
2026-05-21 23:03:58 +1000

    Add the calculation of the arithmetic mean

--- Python/table.py
+++ Python/table.py
@@ -13,7 +13,13 @@ if columns <= 0 or rows <= 0: #Checking for the correct number
 	print("incorrect input, incorrect number entered")
 	sys.exit() #Emergency completion
 
+total_sum = 0
+
 for i in range(0, columns * rows): #Cycle of adding numbers to a table and a range of numbers
 	print(f"{i : 5}", end="")
 	if ((i + 1) % columns == 0): #Checking for moving to a new row
 		print()
+	total_sum += i #Calculating the total sum of the numbers
+
+average = total_sum/(columns * rows) #Calculating the arithmetic mean
+print(f"The arithmetic mean: {average}")
