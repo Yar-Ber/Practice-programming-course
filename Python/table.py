e86fde9ffa988263e64b515a9c3a2b9e812092a4
Yar-Ber <sivachev15@gmail.com>
2026-05-22 20:40:22 +1000

    add a list to efficiently calculate the sum and average

--- Python/table.py
+++ Python/table.py
@@ -13,13 +13,17 @@ if columns <= 0 or rows <= 0: #Checking for the correct number
 	print("incorrect input, incorrect number entered")
 	sys.exit() #Emergency completion
 
-total_sum = 0
+numbers = [] #List
 
 for i in range(0, columns * rows): #Cycle of adding numbers to a table and a range of numbers
 	print(f"{i : 5}", end="")
 	if ((i + 1) % columns == 0): #Checking for moving to a new row
 		print()
-	total_sum += i #Calculating the total sum of the numbers
+	numbers.append(i) #Add the number i to the list
 
-average = total_sum/(columns * rows) #Calculating the arithmetic mean
+
+print(f"Generated list: {numbers}")
+total_sum = sum(numbers)
+print(f"The sum of the numbers: {total_sum}")
+average = total_sum/len(numbers) #Calculating the arithmetic mean
 print(f"The arithmetic mean: {average}")
